cf17905561e6bcb3a393edae611fdeefef67b195
Thor Correia <thor.lucas@berkeley.edu>
2021-12-12 12:01:46 -0800

    tested unsafe cast vs the u64::from_le_bytes
    Turns out the compiler is smart enough to optimize that away. No
    difference.

--- bench/benches/batched.rs
+++ bench/benches/batched.rs
@@ -8,34 +8,50 @@ pub fn buf_benchmark_batched_buffer(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("batched");
     for size in sizes.iter() {    
-        group.throughput(Throughput::Bytes(*size as u64));
-        group.bench_with_input(BenchmarkId::new("batched_u64", size), size, |b, size: &usize| {
-            let size = *size;
-            let buf = &rand_buffer(size)[..];
+        let buf = &rand_buffer(*size)[..];
+        let to = &rand_buffer(*size)[..];
 
+        group.throughput(Throughput::Bytes(*size as u64));
+
+        group.bench_with_input(BenchmarkId::new("batched_u64", size), size, |b, size: &usize| {
             b.iter(|| {
-                let mut i = 0;
-                let mut acc: u64 = 0;
-                while i < size {
-                    acc ^= read_u64_inline(buf, i);
-                    i += 8;
+                let mut acc: usize = 0;
+                for i in (0..*size).step_by(8) {
+                    if read_u64_inline(buf, i) == read_u64_inline(to, i) {
+                        for j in i..i+8 {
+                            if buf[j] == to[j] {
+                                acc += 1;
+                            }
+                        }
+                    }
                 }
-                let mut out: u8 = 0;
-                for b in u64::to_le_bytes(acc) {
-                    out ^= b;
+                acc
+            });
+        });
+
+        group.bench_with_input(BenchmarkId::new("batched_u64_unsafe", size), size, |b, size: &usize| {
+            b.iter(|| {
+                let mut acc: usize = 0;
+                for i in (0..*size).step_by(8) {
+                    if read_u64_unsafe_inline(buf, i) == read_u64_unsafe_inline(to, i) {
+                        for j in i..i+8 {
+                            if buf[j] == to[j] {
+                                acc += 1;
+                            }
+                        }
+                    }
                 }
-                out
+                acc
             });
         });
 
         group.bench_with_input(BenchmarkId::new("per_byte", size), size, |b, size: &usize| {
-            let size = *size;
-            let buf = &rand_buffer(size)[..];
-
             b.iter(|| {
-                let mut acc: u8 = 0;
-                for b in buf {
-                    acc ^= *b;
+                let mut acc: usize = 0;
+                for i in 0..*size {
+                    if buf[i] == to[i] {
+                        acc += 1;
+                    }
                 }
                 acc
             });
